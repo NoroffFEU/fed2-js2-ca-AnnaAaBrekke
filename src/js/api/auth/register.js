7dc15674779e167cb20a8d800677c2cad09330cc
AnnaAaBrekke <annabrekke98@gmail.com>
2024-09-11 09:03:30 +0200

    Authentication for register (without a class for now)

--- src/js/api/auth/register.js
+++ src/js/api/auth/register.js
@@ -1,3 +1,5 @@
+import { API_AUTH_REGISTER } from "../constants.js";
+
 export async function register({
   name,
   email,
@@ -5,4 +7,31 @@ export async function register({
   bio,
   banner,
   avatar,
-}) {}
+}) {
+  const body = JSON.stringify({
+    name,
+    email,
+    password,
+    bio,
+    banner,
+    avatar,
+  });
+
+  const response = await fetch(API_AUTH_REGISTER, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "post",
+    body,
+  });
+
+  if (response.ok) {
+    const { data } = await response.json();
+    // Store JWT token in localStorage
+    localStorage.setItem("token", data.token);
+    return data;
+  }
+
+  // Handle error and display appropriate error message
+  throw new Error("Could not register account")
+}

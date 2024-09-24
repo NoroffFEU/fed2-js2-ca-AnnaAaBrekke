0b4dc2bdbe67e020460a02731ac75ac54831d4f0
AnnaAaBrekke <annabrekke98@gmail.com>
2024-09-24 11:10:40 +0200

    auth.js

--- src/js/router/views/auth.js
+++ src/js/router/views/auth.js
@@ -1,1 +1,7 @@
-alert("Auth Index Page");
+import { authGuard } from "../../utilities/authGuard.js"; // Ensure path is correct
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (authGuard()) {
+    window.location.href = "/profile/";
+  }
+});

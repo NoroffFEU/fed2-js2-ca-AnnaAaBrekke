d1ea4ffaacc010f69004e8a6e412ceea787dd110
AnnaAaBrekke <annabrekke98@gmail.com>
2024-09-24 11:10:13 +0200

    imported router correctly

--- src/app.js
+++ src/app.js
@@ -1,22 +1,5 @@
-// import { router } from "./js/router/index.js";
-// import { setLogoutListener } from "./js/ui/global/logout.js";
-
-// // await router(window.location.pathname);
-
-// // import as posts from "../post/index.js"
-// // posts.createpost/update.. (if i have index.file where i export everythihg)
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   // Check if the logout button exists in the DOM before setting the listener
-//   if (document.getElementById("logout-button")) {
-//     setLogoutListener(); // Initialize the logout button listener only if the button exists
-//   }
-// });
-
-// app.js
-
 import { getKey } from "./js/api/auth/key.js";
-// import { router } from "./js/router/index.js";
+import router from "./js/router/index.js";
 import { setLogoutListener } from "./js/ui/global/logout.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
@@ -26,7 +9,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     console.log("API Key generated and stored:", apiKey);
 
     // Initialize the router to handle the initial page load
-    // await router(window.location.pathname);
+    await router(window.location.pathname);
 
     // Check if the logout button exists in the DOM before setting the listener
     if (document.getElementById("logout-button")) {

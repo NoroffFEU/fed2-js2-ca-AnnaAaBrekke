e1e432bec32be913a62dd0018991fa513bcf71bb
AnnaAaBrekke <annabrekke98@gmail.com>
2024-09-24 14:32:34 +0200

    loader added

--- src/js/router/views/auth.js
+++ src/js/router/views/auth.js
@@ -1,7 +1,13 @@
-import { authGuard } from "../../utilities/authGuard.js"; // Ensure path is correct
+import { authGuard } from "../../utilities/authGuard.js"; // Ensure the path is correct
+import { showLoader, hideLoader } from "../../ui/global/loader.js"; // Import showLoader and hideLoader
 
 document.addEventListener("DOMContentLoaded", () => {
+  showLoader();
+
+  // Perform the auth check
   if (authGuard()) {
     window.location.href = "/profile/";
   }
+
+  hideLoader();
 });

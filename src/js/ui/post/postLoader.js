7e421a91fe01f8ddf3e1035b944b4079711dec71
AnnaAaBrekke <annabrekke98@gmail.com>
2024-09-25 15:51:30 +0200

    load posts

---
+++ src/js/ui/post/postLoader.js
@@ -0,0 +1,22 @@
+import { showLoader, hideLoader } from "../../ui/global/loader.js";
+import { showErrorAlert } from "../../ui/global/alertHandler.js";
+import { displayPosts } from "../../router/views/posts.js";
+import PostService from "../../api/post/postService.js";
+
+const postService = new PostService();
+
+export async function loadPosts(limit = 12, page = 1) {
+  try {
+    showLoader();
+
+    const posts = await postService.fetchPosts({ limit, page });
+    console.log("Posts fetched:", posts);
+
+    displayPosts(posts);
+  } catch (error) {
+    showErrorAlert("Error loading posts.");
+    console.error("Error loading posts:", error.message);
+  } finally {
+    hideLoader();
+  }
+}

64bdc17fc56f61fb3b565e338c09e32b95079a70
AnnaAaBrekke <annabrekke98@gmail.com>
2024-09-28 11:59:09 +0200

    auth

--- src/js/router/views/auth.js
+++ src/js/router/views/auth.js
@@ -1,12 +1,6 @@
-import { authGuard } from "../../utilities/authGuard.js";
 import { showLoader, hideLoader } from "../../ui/global/loader.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   showLoader();
-
-  if (authGuard()) {
-    window.location.href = "/profile/";
-  }
-
   hideLoader();
 });

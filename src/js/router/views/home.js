d172a0ffe95bb3334c45cca79350764e4b27d569
AnnaAaBrekke <annabrekke98@gmail.com>
2024-09-28 09:57:36 +0200

    home if, else statements

--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -2,6 +2,10 @@ import { loadPosts } from "../../ui/post/postLoader.js";
 import { authGuard } from "../../utilities/authGuard.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  authGuard();
-  loadPosts();
+  if (authGuard()) {
+    // If authenticated, load the posts
+    loadPosts();
+  } else {
+    console.log("User is not authenticated. Redirecting to login...");
+  }
 });

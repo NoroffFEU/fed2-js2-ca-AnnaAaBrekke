b9c40f2359312d2d327f1149beda68dbb1456abc
AnnaAaBrekke <annabrekke98@gmail.com>
2024-10-22 14:34:48 +0200

    Show Edit/Delete Buttons Based on Post Ownership including author

--- src/js/ui/post/delete.js
+++ src/js/ui/post/delete.js
@@ -27,15 +27,6 @@ export async function onDeletePost(event) {
     showSuccessAlert(`Post with ID ${postId} has been deleted successfully!`);
     window.location.href = "/";
   } catch (error) {
-    if (error.message.includes("403")) {
-      showErrorAlert(
-        "You do not have permission to delete this post because it is not your post.",
-      );
-    } else {
-      // Log the error silently without showing an alert
-      throw new Error(
-        `Failed to delete post with ID ${postId}: ${error.message}`,
-      );
-    }
+    showErrorAlert(`Failed to delete post: ${error.message}`);
   }
 }

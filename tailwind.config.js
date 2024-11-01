604a00a09a3baca8f8b912c9b259ae7e81f29020
AnnaAaBrekke <annabrekke98@gmail.com>
2024-11-01 10:54:25 +0100

    Working on auth landing page, struggling with extend bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,16 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{html,js}", // Only add extensions you use
+    "./src/**/*.{html,js}", // Adjust extensions based on project file types
     "!./node_modules/**/*",
   ],
   theme: {
     extend: {
       screens: {
-        xs: "270px",
+        xs: "270px", // Custom breakpoint for extra small screens
+      },
+      backgroundImage: {
+        firework: "url('/src/images/sofia-tang-nFQsWCdcL-E-unsplash.jpg')", // Cusnpm run tom background image
       },
     },
   },

1707beb6095a5462a09664c5817c8ff2798fea68
AnnaAaBrekke <annabrekke98@gmail.com>
2024-11-04 10:04:22 +0100

    Form padding and margin

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,12 @@ export default {
       backgroundImage: {
         firework: `url("./src/images/sofia-tang-nFQsWCdcL-E-unsplash.jpg")`,
       },
+      spacing: {
+        "small-padding": "0.625rem", // Use instead of '2.5'
+        "medium-padding": "1.25rem", // Custom spacing as an example
+        "large-padding": "1.875rem", // Another example
+        "input-padding": "2rem", // Custom padding value for form inputs
+      },
     },
   },
   plugins: [],
